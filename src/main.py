aa58b0f0bc6826163c92c8a39e316958a5638df3
Phelipe95 <phelipediogo@hotmail.com>
2025-08-03 13:32:51 -0300

    Finalização do protótipo do projeto, ele está operando com no máximo $10, foi incluído o arquivo de log onde ficará registrado tudo.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 import asyncio
+import logging
 from config.settings import SYMBOL, TIMEFRAME, LIMIT
-from src.utils.binance_client import get_klines, test_connection, test_account, buy_solana, sell_solana
+from src.utils.binance_client import get_klines, test_connection, test_account, buy_solana, sell_solana, client
 from src.utils.telegram_bot import send_message
 from src.strategies.moving_average import check_signal
 from src.utils.indicators import moving_averages
+
+logging.basicConfig(
+    filename="trade_bot.log", # Arquivo de saída
+    level=logging.INFO, # Nível mínimo: INFO
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 async def main():
     if not test_connection():
@@ -12,6 +20,8 @@ async def main():
         return
     
     last_signal = None # evita repetir ordens
+    last_buy_price = None # preço da última compra
+    last_buy_qty = None # quantidade comprada
 
     while True:
         try:
@@ -21,18 +31,35 @@ async def main():
 
             # Verifica sinal
             signal = check_signal(df)
+            price = float(client.get_symbol_ticker(symbol=SYMBOL)["price"])
 
             if signal and signal != last_signal:
                 if signal == "buy":
                     order = buy_solana(SYMBOL)
-                    msg = f"🚀 COMPRA executada em {SYMBOL} (máx $10)"
-                elif signal == "sell":
+                    if order: # pega a quantidade comprada e o preço
+                        executed_qty = float(order["esecutedQty"])
+                        last_buy_qty = executed_qty
+                        last_buy_price = price
+                        msg = f"🚀 COMPRA executada em {SYMBOL} a {price:.2f} (máx $10)"
+                elif signal == "sell" and last_buy_price:
                     order = sell_solana(SYMBOL)
-                    msg = f"🔻 VENDA executada em {SYMBOL} (saldo total)"
+                    if order:
+                        executed_qty = float(order["esecutedQty"])
+                        sell_value = executed_qty * price
+                        buy_value = last_buy_qty * last_buy_price
+                        profit = sell_value - buy_value
+                        pct = (profit / buy_value) * 100 if buy_value > 0 else 0
+                        msg = (
+                            f"🔻 VENDA executada em {executed_qty:.2f} {SYMBOL} a {price:.2f} USDT (saldo total)\n"
+                            f"💰 Resultado: {profit:.2f} USDT ({pct:+.2f}%)"
+                        )
+                        last_buy_price = None
+                        last_buy_qty = None
                 else:
-                    msg = "⏸️ Nenhum sinal — HOLD"
+                    msg = f"⏸️ ({SYMBOL}) Aguardar! Preço atual:  {price:.2f} USDT"
                 
                 print(msg)
+                logging.info(msg)
                 await send_message(msg)
 
                 last_signal = signal
